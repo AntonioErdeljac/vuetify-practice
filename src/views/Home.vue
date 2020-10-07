2f42e2d0bd8731ab43e5a01c961906553afb7add
Antonio Erdeljac <hedhogs@gmail.com>
2020-10-08 01:03:28 +0200

    build: add text, colors and font properties to home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,24 @@
+/* eslint-disable max-len */
 <template>
   <div class="home">
     <h1>Homepage</h1>
+    <p class="red white--text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean porta sodales augue</p>
+    <p class="pink lighten-4 text--darken-4 red--text">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam cumque nisi delectus facere ab ipsum quaerat non enim deserunt laboriosam, rem obcaecati voluptatem dolore, commodi est fugit quisquam consequuntur omnis?
+    </p>
+    <h1 class="display-4">
+      Massive display
+    </h1>
+    <h4 class="display-1">
+      Small display
+    </h4>
+    <p class="headline">
+      This is a headline
+    </p>
+    <p class="subheading font-weight-bold">
+      Subheading
+    </p>
+    <p class="caption">Caption</p>
   </div>
 </template>
 
